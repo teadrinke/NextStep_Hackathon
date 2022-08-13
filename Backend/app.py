88af7816b842d51d55d479c55db85fe6a7c8f96d
Gaurav <gauravsomani52750@gmail.com>
2022-08-13 22:25:52 +0530

    tried integrating react & flask

--- Backend/app.py
+++ Backend/app.py
@@ -1,12 +1,20 @@
 from flask import Flask, request
+from flask_cors import CORS, cross_origin
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+CORS(app, support_credentials=True)
 
-
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
+@cross_origin(supports_credentials=True)
 def index() :
-    return {"title" : "Hackathon", "people" : 4}
+    if request.method == 'POST':
+        print("request:", request.json['body']['name'])
+        # name = request.json['body']['name']
+        # email = request.json['body']['email']
+        # return {"Name" : name, "Email" : email}
+
+    return {"message" : "Please send name and email!"}
 
 
 
